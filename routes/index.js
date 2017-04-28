c42f0ddf02f7c9f969feba6a32fab89d00b09b93
Julian Centurion <julian.centurion@mercadolibre.com>
2017-04-28 12:25:23 -0300

    Send html on response

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var open = require('open');
+//
+
+var html = '<!DOCTYPE HTML>' +
+    '<html lang="en-US">' +
+    '<head>' +
+    '<meta charset="UTF-8">' +
+    '<meta http-equiv="refresh" content="1; url=http://example.com">' +
+    '<script type="text/javascript">' +
+    'window.location.href = "meli://search/input"' +
+    '</script>' +
+    '<title>Page Redirection</title>' +
+    '</head>' +
+    '<body>' +
+    '<!-- Note: don\'t tell people to `click` the link, just tell them that it is a link. -->' +
+    'If you are not redirected automatically, follow this <a href=\'meli://search/input\'>link to example</a>.' +
+    '</body>' +
+    '</html>';
 
 router.get('/', function(req, res){
-    res.send('meli://search/input');
+    res.send(html);
     // res.redirect('https://articulo.mercadolibre.com.ar');
 });
 
