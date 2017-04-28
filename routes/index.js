d8ce4afa2dda16727d4ae521ffa73e5fc6288bd4
Julian Centurion <julian.centurion@mercadolibre.com>
2017-04-28 11:55:26 -0300

    Create project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var open = require('open');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+    res.send('meli://search/input');
+    // res.redirect('https://articulo.mercadolibre.com.ar');
+});
+
+router.get('/deeplink', function(req, res){
+    res.redirect('meli://search/input');
+});
+
+router.get('/weblink', function(req, res){
+    res.redirect('https://celulares.mercadolibre.com.ar');
 });
 
 module.exports = router;
